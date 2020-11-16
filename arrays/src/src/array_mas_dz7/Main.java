30de3c7bf16cc390fe40dc9e96032a3f33c9adc4
Shpakk33 <nata50827@gmail.com>
2020-11-16 23:24:07 +0200

    Hw.16.11

--- arrays/src/src/array_mas_dz7/Main.java
+++ arrays/src/src/array_mas_dz7/Main.java
@@ -3,15 +3,14 @@ package array_mas_dz7;
 public class Main {
     public static void main(String[] args) {
         int n = 4;
-        int [][] a = new int[n][n];
         for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++){
-                System.out.print((j%2==0 ^ i%2==0? 0 : 1) + " ");
+                System.out.print(( j % 2 == 0 ^ i % 2 == 0? 0 : 1) + " ");
 
             }
-        System.out.println();}
+        System.out.println();
         }
-
+        }
 
     }
 
