2c2a29c13346e4b82a6e44660f84ab488947269d
Shpakk33 <nata50827@gmail.com>
2020-12-15 18:38:36 +0200

    Hw_15.12

--- inCaps/src/Calculator/Controller.java
+++ inCaps/src/Calculator/Controller.java
@@ -5,20 +5,22 @@ public class Controller {
         int sum = a + b;
         return sum;
     }
+
     int dif(int a, int b) {
         return a - b;
     }
+
     int mul(int a, int b) {
         return a * b;
     }
-    double div(int a, int b){
-        if (b !=0){
-            return (double) a/b;
 
-        }else{
+    double div(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
             return 0;
+
+
         }
-
-
     }
 }
--- inCaps/src/Calculator/Main.java
+++ inCaps/src/Calculator/Main.java
@@ -1,1 +1,35 @@
 
+package Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Model m1 = new Model();
+        m1.x = 10;
+        m1.y = 2;
+        Model m2 = new Model();
+        m2.x = 100;
+        m2.y = 50;
+
+        Controller c = new Controller();
+        View v = new View();
+
+        int sum = c.add(m1.x, m1.y);
+        System.out.print("cумма = ");
+        v.displayInteger(sum);
+
+        int dif = c.dif(m1.x, m1.y);
+        System.out.print("разница = ");
+        v.displayInteger(dif);
+
+        int mul = c.mul(m2.x, m2.y);
+        System.out.print("произведение = ");
+        v.displayInteger(mul);
+
+        double div = c.div(m2.x, m2.y);
+        System.out.print("частное = ");
+        v.displayDouble((int) div);
+
+
+
+    }
+}
