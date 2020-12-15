6ab96bfd366e3be1c98ae16591a28a615b3b6222
Shpakk33 <nata50827@gmail.com>
2020-12-15 17:52:29 +0200

    Hw_15.12

--- inCaps/src/FractionCalculator/Calculator.java
+++ inCaps/src/FractionCalculator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     static Fraction add(Fraction f1, Fraction f2) {
         Fraction summa = new Fraction();
         summa.denominator = f1.denominator * f2.denominator;
-        summa.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+        summa.numerator = (f1.numerator * f2.denominator) + (f2.numerator * f1.denominator);
         return summa;
     }
 
